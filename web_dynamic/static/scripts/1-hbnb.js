7501da7fd80476a119ab17491f1e27e26b11127c
NicoBelen <81532370+nicobelen@users.noreply.github.com>
2022-09-15 17:43:04 -0300

    404 not found

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 //Write a JavaScript script (static/scripts/1-hbnb.js)
 
-const button_listener = document.getElementById('.button');
-const checks_listener = document.querySelectorAll('.checks');
-
-button_listener.addEventListener('click', function() {
-    checks.forEach((e)=>{
-        console.log(e.value);
-    });
+addEventListener('DOMContentLoaded', () => {
+    const button_listener = document.getElementById('.button');
+    const checks_listener = document.querySelectorAll('.checks');
+    
+    button_listener.addEventListener('click', function() {
+        checks_listener.forEach((e)=>{
+            console.log(e.value);
+        });
+});
 });
